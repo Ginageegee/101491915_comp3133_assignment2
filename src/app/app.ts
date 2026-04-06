56cffb44178d58f0ffa19baf5f15891d983f10fb
Ginageegee <regina.slonimsky@hotmail.com>
2026-04-06 18:44:17 -0400

    initial commit

---
+++ src/app/app.ts
@@ -0,0 +1,12 @@
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App {
+  protected readonly title = signal('studentid101491915-comp3133-assignment2');
+}
